9a1d76ae28f9d20fe21bc9b95989003a6054197c
KattosDev <130821327+KattosDev@users.noreply.github.com>
2023-04-15 08:25:07 +0200

    Add files via upload

---
+++ kattos-parachute/client.lua
@@ -0,0 +1,33 @@
+--Context Menu--
+Citizen.CreateThread(function()
+    local options = {}
+
+    for k, v in pairs(Config.Parachuteglide) do
+        table.insert(options, {
+            title = v[1],
+            onSelect = function() Jump(v[2], v[3], v[4]) end
+        })
+    end
+
+    lib.registerContext({
+        id = 'kattos_parachute',
+        title = Config.Title,
+        options = options
+    })
+end)
+
+--Command--
+RegisterCommand("parachute", function()
+    lib.showContext('kattos_parachute')
+end)
+
+--Gomb amivel meg nyitod--
+RegisterKeyMapping('parachute', 'Open parachute Menu', 'keyboard', 'HOME')
+
+--Amikor ki ugrasz ezt kapod--
+function Jump(x, y, z)
+	SetEntityCoords(PlayerPedId(), x, y, z)
+	TriggerServerEvent('addParachute')
+	GiveWeaponToPed(PlayerPedId(), GetHashKey('gadget_parachute'), 1, true, true)
+end
+
---
+++ kattos-parachute/config.lua
@@ -0,0 +1,12 @@
+Config = {}
+
+Config.Title = "Kattos Parachute" --Ez lenne a Fejléce a Context-nek
+
+Config.Parachuteglide = { -- Itt tudod kezelni a kordinátákat hova dobjon.
+	{"Varos", 302.9461, -241.9427, 157.2754},
+    {"Paleto", 317.6257, -207.7952, 154.0863},
+	{"Allami", 317.6257, -207.7952, 154.0863}
+}
+
+
+
